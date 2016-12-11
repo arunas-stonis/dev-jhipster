0ff1cec356c06099729ff6cf8edfee0e35065f22
arunas <arunas.stonis@gmail.com>
2016-12-11 12:56:19 +0200

    generated

---
+++ src/main/webapp/app/app.module.ts
@@ -0,0 +1,68 @@
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { UIRouterModule } from 'ui-router-ng2';
+import { Ng2Webstorage } from 'ng2-webstorage';
+
+import { MyappSharedModule } from './shared';
+import { MyappAdminModule } from './admin/admin.module'; //TODO these couldnt be used from barrels due to an error
+import { MyappAccountModule } from './account/account.module';
+
+import { appState } from './app.state';
+import { HomeComponent, homeState } from './home';
+import { JhiRouterConfig } from './blocks/config/router.config';
+import { localStorageConfig } from './blocks/config/localstorage.config';
+import { customHttpProvider } from './blocks/interceptor/http.provider';
+
+import {
+    JhiMainComponent,
+    NavbarComponent,
+    FooterComponent,
+    ProfileService,
+    PageRibbonComponent,
+    ActiveMenuDirective,
+    ErrorComponent,
+    errorState,
+    accessdeniedState
+} from './layouts';
+
+localStorageConfig();
+
+let routerConfig = {
+    configClass: JhiRouterConfig,
+    useHash: true,
+    states: [
+        appState,
+        homeState,
+        errorState,
+        accessdeniedState
+    ]
+};
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        UIRouterModule.forRoot(routerConfig),
+        Ng2Webstorage,
+        MyappSharedModule,
+        MyappAdminModule,
+        MyappAccountModule
+    ],
+    declarations: [
+        JhiMainComponent,
+        HomeComponent,
+        NavbarComponent,
+        ErrorComponent,
+        PageRibbonComponent,
+        ActiveMenuDirective,
+        FooterComponent
+    ],
+    providers: [
+        ProfileService,
+        { provide: Window, useValue: window },
+        { provide: Document, useValue: document },
+        customHttpProvider()
+    ],
+    bootstrap: [ JhiMainComponent ]
+})
+export class MyappAppModule {}
---
+++ src/main/webapp/app/entities/entity.module.ts
@@ -0,0 +1,27 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { UIRouterModule } from 'ui-router-ng2';
+
+import { MyappSharedModule } from '../shared';
+
+
+let ENTITY_STATES = [
+
+];
+
+@NgModule({
+    imports: [
+        MyappSharedModule,
+        UIRouterModule.forChild({ states: ENTITY_STATES })
+    ],
+    declarations: [
+
+    ],
+    entryComponents: [
+
+    ],
+    providers: [
+
+    ],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class MyappEntityModule {}
